64cac6f758f0c98700decc8c128266fbb345e8c2
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-03 17:34:47 -0300

    feat: :sparkles: add database config

---
+++ config/database.ts
@@ -0,0 +1,10 @@
+import app from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.listen(process.env.PORT, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
+});
