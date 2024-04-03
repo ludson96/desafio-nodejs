4384010a428bae027988c3ce1f36b520c5612f66
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-03 19:01:34 -0300

    feat: :sparkles: add .env.example

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,17 @@
-import index from "../src/index"
-import dotenv from 'dotenv';
+import index from '../src/index';
+import mysql, { ConnectionOptions } from 'mysql2/promise';
+import 'dotenv/config'
 
-dotenv.config();
-
+const connection: ConnectionOptions = {
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: (process.env.MYSQL_PORT ?? 3306) as number, 
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.DATABASE || 'db',
+};
 
 index.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
+
+export default mysql.createConnection(connection);
