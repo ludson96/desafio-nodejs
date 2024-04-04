8d5261df15c6fca5da9627939a56b77a22494ab5
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-04 11:40:53 -0300

    feat: :recycle: database new sequelize

--- config/database.ts
+++ config/database.ts
@@ -1,38 +1,20 @@
 import 'dotenv/config';
+import { Sequelize } from 'sequelize-typescript';
 
-type Environment = 'production' | 'development' | 'test';
-
-const environment = process.env.NODE_ENV || 'test';
-
-const suffix = {
-  production: '-prod',
-  development: '-dev',
-  test: '-test',
-};
-
-const databaseSuffix = suffix[environment as Environment] || suffix.test;
-
-const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: `${process.env.MYSQL_DB_NAME || 'testedb'}${databaseSuffix}`,
-  dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
+const sequelize = new Sequelize(
+  {
+    database: `${process.env.MYSQL_DB_NAME || 'schedule'}`,
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || 'root',
+    dialect: 'mysql',
+    dialectOptions: {
+      timezone: 'Z',
+    },
+    logging: false,
+    models: [`${__dirname}/models`],
   },
-  logging: false,
-};
+);
 
-export default {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
-  },
-};
+export default sequelize;
